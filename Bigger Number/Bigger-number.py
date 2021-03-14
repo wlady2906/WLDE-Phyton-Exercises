b83da20115cc22898f155d86ea3e2a0aa49fd58a
Wladimir De la S Macías <75133031+wlady2906@users.noreply.github.com>
2021-03-14 10:19:57 -0500

    Create Bigger-number.py

---
+++ Bigger Number/Bigger-number.py
@@ -0,0 +1,18 @@
+import os
+import math
+print("Print the bigger number by not using the reserved function max(). Declare a function (def) which execute the same logic")
+
+
+def my_max(x,y):
+    if (x < y):
+        txt = "The bigger number is {}"
+        print(txt.format(y))
+    
+    if (x > y):
+        txt = "The bigger number is {}"
+        print(txt.format(x))
+
+arg1 = int(input("Introduce Number 1 --> "))
+arg2 = int(input("Introduce number 2 --> "))
+
+my_max(arg1,arg2)
