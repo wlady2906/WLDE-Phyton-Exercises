6a8994536ddf8fbe59ce8343ad72b0d9ad7c61f5
Wladimir De la S Macías <75133031+wlady2906@users.noreply.github.com>
2021-03-14 11:25:21 -0500

    Update AnnualRate.py

--- InterestRate/AnnualRate.py
+++ InterestRate/AnnualRate.py
@@ -1,3 +1,15 @@
+'''
+
+Programa que pida al usuario una cantidad de dolares, una tasa de interés y un numero de años. 
+Muestra por pantalla en cuanto se habrá convertido el capital inicial transcurridos esos años 
+si cada año se aplica la tasa de interés introducida.
+Recordar que un capital C dolares a un interés del x por cien durante n años se convierte en 
+C * (1 + x/100)elevado a n (años). 
+Probar el programa sabiendo que una cantidad de 10000 dolares al 4.5% de interés anual 
+se convierte en 24117.14 dolares al cabo de 20 años.
+
+'''
+
 import math
 
 print ('Calcula la inversión con un interes anual')
