5be58517ede3bebd1e28d3a214570a1d5096a0e5
Wladimir De la S Macías <75133031+wlady2906@users.noreply.github.com>
2021-03-14 10:52:41 -0500

    Delete Searching trough a file.py

--- Search criteria/Searching trough a file.py
+++
@@ -1,8 +0,0 @@
-print ('Usar un criterio de busqueda para todas las lineas')
-archivo = open("ZOOM ID.txt")
-
-for search in archivo:
-    search = search.rstrip()
-    if not 'AGUIRRE MUNIZAGA' in search:
-        continue
-    print(search)
