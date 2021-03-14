15ea16101cdab0d124c483adedcacd7fba2ccd0c
Wladimir De la S Macías <75133031+wlady2906@users.noreply.github.com>
2021-03-14 11:34:44 -0500

    Create BinaryToInt.py

---
+++ FormBinaryToInteger/BinaryToInt.py
@@ -0,0 +1,11 @@
+'''
+
+Construir un pequeño programa que convierta números binarios en enteros.
+
+'''
+print ('Convierte numeros binarios en enteros')
+
+x = input('Introduce un numero en binario ')
+entero = int(x, base=2)
+
+print(entero)
