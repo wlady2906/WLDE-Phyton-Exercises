0cc3d49b1834ea38b24bbf05e652ef925e7ee832
Wladimir De la S Macías <75133031+wlady2906@users.noreply.github.com>
2021-03-14 11:03:57 -0500

    Create Bigger number in a list.py

---
+++ Bigger Number in a List/Bigger number in a list.py
@@ -0,0 +1,10 @@
+import os
+import math
+def max_in_list(lista):
+        maximo = max(lista)
+        print(maximo)
+
+#Definimos una lista con números llenos
+lista = [6,7,89,67,12,34,56,8,12,1,0]
+
+max_in_list(lista)
